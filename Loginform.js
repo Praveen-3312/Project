0290a5b2da7964cc4e28e6958f97575bfd32e285
Praveen-3312 <117912083+Praveen-3312@users.noreply.github.com>
2022-11-10 17:20:31 +0530

    Add files via upload

---
+++ Loginform.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Loginform.css';
+
+const Loginform=(props)=>{
+    return(
+        
+        <div className="Loginform">
+            {/* <label>Name</label> */}
+            <input  name={props.name} placeholder={props.placeholder}/>  
+              </div>
+        
+    )
+}
+export default Loginform;
+       
